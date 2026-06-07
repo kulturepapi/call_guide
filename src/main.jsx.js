34c84b0c5d4d68c067463a961337a3cc75a79171
kulturepapi <152605666+kulturepapi@users.noreply.github.com>
2026-06-07 20:01:08 +0200

    Add files via upload

---
+++ src/main.jsx.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CallGuide from './fonio_call_guide.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CallGuide />
+  </React.StrictMode>
+)
